c5ab5d9171b28e4d87864b0490276a9b08c04da9
HuyNQ212 <quanghuy21299@gmail.com>
2023-01-07 21:57:06 +0700

    update code for feature-test

--- Demo-App/src/main/java/org/example/Main.java
+++ Demo-App/src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         System.out.println("Something else to push!");
+        int a = 10;
+        int b =5;
+        System.out.println(a+b);
     }
 }
